872d8d08c4a1913335e468b73abcb6e1c94727ac
illumiko <eyanat.rayannjr@gmail.com>
2025-02-23 13:30:35 +0600

    xcape adjustment

--- wms/qtile/autostart.sh
+++ wms/qtile/autostart.sh
@@ -5,6 +5,7 @@ xrandr --output eDP --gamma 1:0.9:0.8 --brightness 0.8 &
 xrandr --output eDP --auto --output HDMI-A-0 --auto --left-of eDP &
 vibrant-cli HDMI-A-0 1.2 &
 vibrant-cli eDP 1.1 &
-xcape -t 1000 -e "Super_L=Super_L|Control_L|b" &
+xcape -t 300 -e "Super_L=Super_L|Control_L|b" &
 syncthing & 
+mpd &
 xmodmap $HOME/dotfiles/wms/x11/xmodmap 
