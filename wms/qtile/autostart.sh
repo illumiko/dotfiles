e5decc5c77079c85a6eba14e302bf4b6e4e8b259
illumiko <eyanat.rayannjr@gmail.com>
2025-02-23 09:58:53 +0600

    qtile, .gitignore
    added pycache dir to gitignore
    qtile fixing picom autostart

--- wms/qtile/autostart.sh
+++ wms/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-picom -b &
+picom -b --config $HOME/dotfiles/wms/picom/picom.conf &
 dunst &
 xrandr --output eDP --gamma 1:0.9:0.8 --brightness 0.8 &
 xrandr --output eDP --auto --output HDMI-A-0 --auto --left-of eDP &
